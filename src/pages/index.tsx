5249e1057f8ddb2f6d2e1cc6ccf3790af3168e03
Niklas <zhoie@icloud.com>
2023-04-10 13:42:04 +0800

    files group test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Sidebar from '../components/Sidebar'
+import Side from '../components/groups/Side'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
     <Sidebar />
+    <Side />
+    </>
   )
 }
