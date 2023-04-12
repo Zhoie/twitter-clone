dcf71a5330b470d7ebc52433c7db8f9d3f28f1bd
Niklas <zhoie@icloud.com>
2023-04-12 15:36:37 +0800

    database

---
+++ hooks/useCurrentUser.ts
@@ -0,0 +1,14 @@
+import userSWR from 'swr'
+
+import fetcher from '../libs/fetcher'
+
+export default function useCurrentUser() {
+    const { data, error, isLoading, mutate } = userSWR('/api/current', fetcher)
+
+    return {
+        data,
+        error,
+        isLoading,
+        mutate
+    }
+}
